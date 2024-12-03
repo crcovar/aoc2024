48076d147bea743bab42884d98f75650bde1b02c
Charles Covar <ccovar@flexgen.com>
2024-12-03 16:38:54 -0500

    Adds alternate approach for part 3

--- day_3/day3.exs
+++ day_3/day3.exs
@@ -1,4 +1,9 @@
 defmodule Day3 do
+  defp mul(x), do: Enum.reduce(x, 1, fn c, a -> String.to_integer(c) * a end)
+
+  defp calc(c, a, true), do: {true, mul(Enum.slice(c, 2, 2)) + a}
+  defp calc(c, a, false), do: {false, a}
+
   def part1 do
     IO.puts("Day3 Part 1")
     regex = ~r/mul\((?<x>\d{1,3}),(?<y>\d{1,3})\)/
@@ -6,9 +11,7 @@ defmodule Day3 do
     File.stream!("./input.txt")
     |> Stream.map(&Regex.scan(regex, &1, capture: :all_names))
     |> Stream.flat_map(& &1)
-    |> Stream.map(fn x ->
-      Enum.reduce(x, 1, fn c, a -> String.to_integer(c) * a end)
-    end)
+    |> Stream.map(fn x -> mul(x) end)
     |> Enum.sum()
     |> IO.puts()
   end
@@ -30,15 +33,33 @@ defmodule Day3 do
       end
 
       if Agent.get(go, & &1) and String.starts_with?(hd(x), "mul") do
-        x |> Enum.slice(2, 2) |> Enum.reduce(1, fn c, a -> String.to_integer(c) * a end)
+        x |> Enum.slice(2, 2) |> mul()
       else
         0
       end
     end)
     |> Enum.sum()
+    |> IO.puts()
+  end
+
+  def part2b do
+    IO.puts("Day3 Part 2")
+    regex = ~r/(mul\((?<x>\d{1,3}),(?<y>\d{1,3})\))|(do\(\))|(don\'t\(\))/
+
+    File.stream!("./input.txt")
+    |> Stream.map(&Regex.scan(regex, &1, capture: :all))
+    |> Stream.flat_map(& &1)
+    |> Enum.reduce({true, 0}, fn c, {go, a} ->
+      case hd(c) do
+        "do()" -> {true, a}
+        "don't()" -> {false, a}
+        _ -> calc(c, a, go)
+      end
+    end)
     |> IO.inspect()
   end
 end
 
 Day3.part1()
 Day3.part2()
+Day3.part2b()
