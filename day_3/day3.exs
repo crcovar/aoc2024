68c760459bdc4c739b1c249af55c10542dd035dc
Charles Covar <ccovar@flexgen.com>
2024-12-03 10:25:30 -0500

    Finished day 3

--- day_3/day3.exs
+++ day_3/day3.exs
@@ -16,19 +16,20 @@ defmodule Day3 do
   def part2 do
     IO.puts("Day3 Part 2")
     regex = ~r/(mul\((?<x>\d{1,3}),(?<y>\d{1,3})\))|(do\(\))|(don\'t\(\))/
-    go = true
+
+    {:ok, go} = Agent.start_link(fn -> true end)
 
     File.stream!("./input.txt")
     |> Stream.map(&Regex.scan(regex, &1, capture: :all))
     |> Stream.flat_map(& &1)
     |> Stream.map(fn x ->
       case hd(x) do
-        "do()" -> go = true
-        "don't()" -> go = false
+        "do()" -> Agent.update(go, fn _ -> true end)
+        "don't()" -> Agent.update(go, fn _ -> false end)
         _ -> nil
       end
 
-      if go and String.starts_with?(hd(x), "mul") do
+      if Agent.get(go, & &1) and String.starts_with?(hd(x), "mul") do
         x |> Enum.slice(2, 2) |> Enum.reduce(1, fn c, a -> String.to_integer(c) * a end)
       else
         0
